1ab5009560d6088de75bdadb4954eaf04e7a636e
Sania-Devv <130513965+Sania-Devv@users.noreply.github.com>
2023-10-07 13:44:47 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,87 @@
+
+//practice 1.1
+/* Open the browser console, type 4 + 10, and press Enter. What do you see as
+the response*/
+//QUESTION1
+// ans is 14
+
+/*Use the console.log() syntax, placing a value within the rounded brackets.
+Try entering your name with quotes around it (this is to indicate the fact that
+it's a text string—we'll get to this in the next chapter).*/
+//QUESTION 2
+
+// console.log(14,"SANIA")
+
+
+  //Practice exercise 1.2
+/*1.Open your code editor and create an HTML file.
+2. Within your HTML file, set up the HTML tags, doctype, HTML, head, and
+body, and then proceed and add the script tags.
+3. Place some JavaScript code within the script tags. You can use console.
+log("hello world!").*/
+  //check in html....
+
+  //Practice exercise 1.3
+
+  /*Linking to a JS JavaScript file:
+
+1. Create a separate file called app with the extension .js.
+2. Within the .js file, add some JavaScript code.
+3. Link to the separate .js file within the HTML file you created in Practice
+exercise 1.2.
+4. Open the HTML file within your browser and check to see whether the
+JavaScript code ran properly.*/
+
+//Practice exercise 1.4
+
+/* 1.Add a new statement to your JavaScript code by setting a variable value.
+Since we will cover this in the next chapter, you can use the following line:
+let a = 10;*/
+let a = 10;
+
+/*2.Add a comment at the end of the statement indicating that you set a value of
+10.*/
+
+//let a = 10;
+
+/*3.Print the value using console.log(). Add a comment explaining what this
+will do.*/
+// console.log(a)
+
+/* 4.At the end of your JavaScript code, use a multiple-line comment. In a real
+production script, you might use this space to add a brief outline of the
+purpose of the file.*/
+
+//Chapter project
+/* 1. In the JavaScript file, output your name into the console and add a multiple-
+line comment to your code.*/
+console.log("Sania Maqsood Ahmed")
+/* Multiple line comment:
+   Multiline comments are used for large text descriptions of code
+  Comments are ignored by the compiler */
+  
+  /* 2.Try commenting out the console message in your JavaScript file so that
+nothing shows in the console.*/
+
+// console.log("Hi!I'm learning JAVASCRIPT")
+      // Self-check quiz
+
+  // 1.What is the HTML syntax to add an external JavaScript file?
+  //ANSWER 1:
+  /*  In the head tag we use script tag and  connect the external js file i.e;
+  <script type="text/javascript" src="app.js"></script>*/
+
+// 2. Can you run JavaScript in a file with a JS extension in your browser?
+//ANSWER 2:
+// Yes, you can run JavaScript code that is stored in a file with a .js extension in your web browser. 
+
+// 3. How do you write a multiple-line comment in JavaScript?
+//ANSWER 3:
+// In JavaScript, you can write multi-line comments using either /* and */
+
+/* 4. What is the best way to remove a line of code from running that you might
+want to keep as you debug?*/
+//ANSWER 4:
+/*  The best way to temporarily remove a line of code from running while you're debugging,
+    without actually deleting it, is to comment it out.  */
+
